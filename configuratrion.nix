3a0c04928b7f357c26d19ccf8aa7b1ad1b33dbf9
Kuaaro <120309479+Kuaaro@users.noreply.github.com>
2024-08-21 17:08:26 +0200

    Create configuratrion.nix

---
+++ configuratrion.nix
@@ -0,0 +1,47 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./bluetooth.nix
+      ./bootloader.nix
+      ./cleaning.nix
+      ./hardware_configuration.nix
+      ./kernel.nix
+      ./mac_randomizer.nix
+      ./networking.nix
+      ./nix-pkgs.nix
+      ./nix-settings.nix
+      ./printing.nix
+      ./programming_languages.nix
+      ./security.nix
+      ./simple_programs.nix
+      ./users.nix
+    ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
