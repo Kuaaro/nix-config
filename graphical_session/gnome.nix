ea0f37f19fd54c900676bb58866da84bfe34022c
= <that.kuaaro@gmail.com>
2024-10-21 10:19:03 +0200

    screen lock

--- graphical_session/gnome.nix
+++ graphical_session/gnome.nix
@@ -4,6 +4,9 @@
     services.xserver = {
         enable = true;
         displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
+        desktopManager.gnome = {
+        	enable = true;
+        	settingsDaemon.plugins.applets.lockStatus.enable = true;
+        };
     };
 }
