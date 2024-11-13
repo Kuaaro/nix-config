74018a4960a10b6f22d3d78073b4ffa4aad08284
kuaaro <that.kuaaro@gmail.com>
2024-11-13 11:38:33 +0100

    tst7

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     #};
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-vscode-extensions, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
   let
   	system = "x86_64-linux";
   in
