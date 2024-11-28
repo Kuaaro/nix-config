72457909058e5518879a7d0c42be86b7872701e0
kuaaro <that.kuaaro@gmail.com>
2024-11-28 13:13:57 +0100

    pyvista

--- home/programming/programming_languages.nix
+++ home/programming/programming_languages.nix
@@ -4,6 +4,14 @@
 	home.packages = with pkgs; [
 		mono
 		texliveFull
-		python3
+		#python312Packages.python
+		#python312Packages.numpy
+		#python312Packages.matplotlib
+		
+		(python312.withPackages(ps: with ps; [ 
+			numpy
+			matplotlib
+			pyvista
+		]))
 	];
 }
